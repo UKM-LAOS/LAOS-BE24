2026d2d416b01db49ab7785c7a38b5b879d5ae46
ChristYoga123 <christianuswibisono@gmail.com>
2024-10-10 10:54:32 +0700

    fix: fix my course migration

--- database/migrations/2024_10_10_035313_add_unique_constraint_to_my_courses.php
+++ database/migrations/2024_10_10_035313_add_unique_constraint_to_my_courses.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Course;
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('my_courses', function (Blueprint $table) {
-            $table->id();
-            $table->foreignIdFor(User::class, 'user_id');
-            $table->foreignIdFor(Course::class, 'course_id');
-            $table->timestamps();
+        Schema::table('my_courses', function (Blueprint $table) {
+            $table->unique(['user_id', 'course_id']);
         });
     }
 
@@ -26,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('my_courses');
+        Schema::table('my_courses', function (Blueprint $table) {
+            //
+        });
     }
 };
